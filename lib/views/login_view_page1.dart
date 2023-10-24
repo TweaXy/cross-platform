0e3a10845eb7ce8799a24ef521216697f1b2302e
kaokb <kaokabkareem@gmail.com>
2023-10-24 15:40:04 +0300

    my_second_commit

--- lib/views/login_view_page1.dart
+++ lib/views/login_view_page1.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tweaxy/components/custom_button.dart';
 import 'package:tweaxy/components/transition/custom_page_route.dart';
 import 'package:tweaxy/components/custom_text_form_field.dart';
+import 'package:tweaxy/constants/custom_text_form_validations.dart';
 import 'package:tweaxy/views/login_view_page2.dart';
 
 class LoginViewPage1 extends StatefulWidget {
@@ -67,6 +68,7 @@ class _LoginViewPage1State extends State<LoginViewPage1> {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15.0),
             child: CustomTextField(
+              validatorFunc: emailValidation,
               label: 'Phone, email address, username',
               controller: myController,
             ),
