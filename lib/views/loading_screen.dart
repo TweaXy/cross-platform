e5b2c9ea637ef31239178ceffa2aea97d97113a4
AhmedSamy02 <https://github.com/AhmedSamy02>
2023-10-24 05:22:44 +0300

    [ ➕ FR ] Adding Loading Screen; Loading Screen Finished

--- lib/views/loading_screen.dart
+++ lib/views/loading_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:loading_indicator/loading_indicator.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoadingScreen extends StatelessWidget {
-  const LoadingScreen({super.key});
-
+  const LoadingScreen({super.key, required this.asyncCall});
+  final bool asyncCall;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ModalProgressHUD(
+      color: Colors.white,
+      opacity: 1,
+      progressIndicator: const SizedBox(
+        height: 65,
+        width: 65,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            LoadingIndicator(
+                indicatorType: Indicator.circleStrokeSpin,
+                colors: [Colors.black]),
+            Icon(
+              Icons.flutter_dash, //TODO: Put the Logo here
+              size: 50,
+              color: Colors.black,
+            ),
+          ],
+        ),
+      ),
+      child: Placeholder(),
+      inAsyncCall: asyncCall,
+    );
   }
 }
