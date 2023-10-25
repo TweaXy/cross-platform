07c0a51378cc714e900774add067cfd129d6d7f7
AhmedSamy02 <https://github.com/AhmedSamy02>
2023-10-26 00:21:54 +0300

    [⚡️ PERF ] Adding Forget Password Screen; Improve appbar.

--- lib/views/forget_password_page2.dart
+++ lib/views/forget_password_page2.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:tweaxy/components/custom_app_bar.dart';
 
 class ForgetPasswordPage2 extends StatelessWidget {
   const ForgetPasswordPage2({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: CustomAppBar(context),
+      body: Column(children: [
+        Text(
+          'Where should we send a confirmation code',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 32,
+          ),
+        ),
+      ]),
+    );
   }
 }
