481fdac738f81f806395a92d732f2650365e1449
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2023-10-26 16:22:30 +0300

    [⚡️ PERF] Converted components to statless widgets

--- lib/components/custom_appbar.dart
+++ lib/components/custom_appbar.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 
-class CustomAppbar extends StatefulWidget {
+class CustomAppbar extends StatelessWidget {
   const CustomAppbar(
       {super.key, required this.iconShape, required this.iconAction});
   final Icon iconShape;
   final Function iconAction;
-  @override
-  State<CustomAppbar> createState() => _CustomAppbarState();
-}
-
-class _CustomAppbarState extends State<CustomAppbar> {
   @override
   Widget build(BuildContext context) {
     return AppBar(
@@ -20,14 +15,15 @@ class _CustomAppbarState extends State<CustomAppbar> {
           child: Image.asset(
             alignment: Alignment.center,
             Brightness.dark == Theme.of(context).brightness
-      ?  'assets/images/logo-light.png':'assets/images/logo-black.png', // Replace with the path to your image
+                ? 'assets/images/logo-light.png'
+                : 'assets/images/logo-black.png', // Replace with the path to your image
             height: 25,
           ),
         ),
       ),
       leading: IconButton(
-        icon: widget.iconShape,
-        onPressed: widget.iconAction(context),
+        icon: iconShape,
+        onPressed: iconAction(context),
       ),
     );
   }
