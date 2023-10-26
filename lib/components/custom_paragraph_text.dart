3b60ac238529f06471a936aee7231cd41e746630
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2023-10-26 19:33:00 +0300

    [➕ FR] adding authentication page

--- lib/components/custom_paragraph_text.dart
+++ lib/components/custom_paragraph_text.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CustomParagraphText extends StatelessWidget {
-  const CustomParagraphText({super.key, required this.textValue});
+  const CustomParagraphText(
+      {super.key, required this.textValue, required this.textAlign});
   final String textValue;
+  final TextAlign textAlign;
   @override
   Widget build(BuildContext context) {
     return Text(
       textValue,
+      textAlign: textAlign,
       overflow: TextOverflow.fade,
       style: const TextStyle(
           fontWeight: FontWeight.w400, fontSize: 20, color: Colors.black54),
