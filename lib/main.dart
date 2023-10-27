035161e2b1d01e43ffb0181389c948e6459e21d2
AhmedSamy02 <https://github.com/AhmedSamy02>
2023-10-28 00:52:48 +0200

    [ ➕ FR ] Adding Starting Screens; Adding Web support to Splash Screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tweaxy/Views/splash_screen.dart';
 import 'package:tweaxy/Views/start_screen.dart';
+import 'package:tweaxy/Views/start_screen_web.dart';
 import 'package:tweaxy/constants.dart';
 
 void main() {
@@ -14,9 +15,11 @@ class MainMaterialApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      
       routes: {
         kSplashScreen: (context) => const SplashScreen(),
         kStartScreen: (context) => const StartScreen(),
+        kWebStartScreen:(context) => const WebStartScreen(),
       },
       initialRoute: kSplashScreen,
     );
