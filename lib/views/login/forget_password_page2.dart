e40fcdbbbe65a163a023c3af09b11dec2bd16738
AhmedSamy02 <https://github.com/AhmedSamy02>
2023-11-05 11:35:24 +0200

    [ 🎨 REF ] Adding views file;

---
+++ lib/views/login/forget_password_page2.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tweaxy/components/custom_appbar.dart';
+import 'package:tweaxy/components/custom_button.dart';
+import 'package:tweaxy/components/transition/custom_page_route.dart';
+import 'package:tweaxy/utilities/theme_validations.dart';
+import 'package:tweaxy/views/login/forget_password_page3.dart';
+
+class ForgetPasswordPage2 extends StatelessWidget {
+  const ForgetPasswordPage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    bool isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    return Scaffold(
+      appBar: CustomAppbar(
+        iconButton: IconButton(
+          icon: Icon(
+            Icons.close_sharp,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(18, 12, 18, 12),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 14.0),
+              child: Text(
+                'Where should we send a confirmation code',
+                style: GoogleFonts.robotoFlex(
+                  color: !isDarkMode ? Colors.black87 : Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 14.0),
+              child: Text(
+                '\nBefore you can change your password, we need to make sure it\'s really you.',
+                style: GoogleFonts.robotoSlab(
+                  color: !isDarkMode ? Colors.black54 : Colors.white24,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 14.0),
+              child: Text(
+                '\nLet\'s text a code to the phone number ending in 47.',
+                style: GoogleFonts.roboto(
+                    color: !isDarkMode ? Colors.black87 : Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            const Spacer(),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CustomButton(
+                    key: const ValueKey('cancelForgetScreen2'),
+                    color: backgroundColorTheme(context),
+                    text: 'Cancel',
+                    onPressedCallback: () {
+                      Navigator.pop(context);
+                    },
+                    initialEnabled: true),
+                CustomButton(
+                    key: const ValueKey('nextForgetScreen2'),
+                    color: forgroundColorTheme(context),
+                    text: 'Next',
+                    onPressedCallback: () {
+                      Navigator.push(
+                          context,
+                          CustomPageRoute(
+                              child: const ForgetPasswordPage3(),
+                              direction: AxisDirection.left));
+                    },
+                    initialEnabled: true),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/views/login/forget_password_page3.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:tweaxy/components/custom_appbar.dart';
+import 'package:tweaxy/components/custom_button.dart';
+import 'package:tweaxy/components/transition/custom_page_route.dart';
+import 'package:tweaxy/components/custom_text_form_field.dart';
+import 'package:tweaxy/utilities/custom_text_form_validations.dart';
+import 'package:tweaxy/utilities/theme_validations.dart';
+import 'package:tweaxy/views/login/forget_password_page2.dart';
+import 'package:tweaxy/views/login/login_view_page2.dart';
+
+class ForgetPasswordPage3 extends StatefulWidget {
+  const ForgetPasswordPage3({super.key});
+
+  @override
+  State<ForgetPasswordPage3> createState() => _LoginViewPage1State();
+}
+
+class _LoginViewPage1State extends State<ForgetPasswordPage3> {
+  TextEditingController myController = TextEditingController();
+  bool isButtonEnabled = false;
+  @override
+  void initState() {
+    super.initState();
+    myController.addListener(_updateButtonState);
+  }
+
+  void _updateButtonState() {
+    setState(() {
+      isButtonEnabled = myController.text.isNotEmpty;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    return Scaffold(
+      appBar: CustomAppbar(
+        iconButton: IconButton(
+          key: const ValueKey("forgetPassView3BackIcon"),
+          icon: Icon(
+            Icons.close_sharp,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Text(
+                  'We sent you a code',
+                  overflow: TextOverflow.clip,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 15.0, left: 15, right: 15),
+            child: Text(
+              'Check your phone to get your confirmation code. if you need to requst a new code, go back and reselect a confimation method.',
+              overflow: TextOverflow.fade,
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+                color: !isDarkMode ? Colors.black87 : Colors.white38,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: CustomTextField(
+              key: const ValueKey("forgetPassView3TextField"),
+              validatorFunc: emailValidation,
+              label: 'Enter your code',
+              controller: myController,
+            ),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child:
+                  Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+                Divider(
+                  color: Colors.black26,
+                  height: 0.5,
+                  thickness: 1,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomButton(
+                        key: const ValueKey("forgetPassView3BackButton"),
+                        color: backgroundColorTheme(context),
+                        text: 'Back',
+                        initialEnabled: true,
+                        onPressedCallback: () {
+                          Navigator.pop(context);
+                          // Navigator.push(
+                          //     context,
+                          //     CustomPageRoute(
+                          //         direction: AxisDirection.left,
+                          //         child: ()));
+                        },
+                      ),
+                      CustomButton(
+                        key: const ValueKey("forgetPassView3NextButton"),
+                        color: forgroundColorTheme(context),
+                        text: 'Next',
+                        initialEnabled: isButtonEnabled,
+                        onPressedCallback: () {
+                          Navigator.pop(context);
+                          Navigator.push(
+                              context,
+                              CustomPageRoute(
+                                  direction: AxisDirection.left,
+                                  child: LoginViewPage2(
+                                    initialValue: myController,
+                                  )));
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ]),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
