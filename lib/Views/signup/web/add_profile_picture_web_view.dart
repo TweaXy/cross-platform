aeb9a97b5fda96b266333e035d7d2a0b065a89e7
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2023-11-08 05:07:18 +0200

    [➕ FR] Added uploading pfp in web signup

--- lib/Views/signup/web/add_profile_picture_web_view.dart
+++ lib/Views/signup/web/add_profile_picture_web_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:tweaxy/Views/signup/web/add_username_web_view.dart';
 import 'package:tweaxy/components/custom_appbar.dart';
 import 'package:tweaxy/components/custom_button.dart';
@@ -6,8 +7,30 @@ import 'package:tweaxy/components/custom_head_text.dart';
 import 'package:tweaxy/components/custom_paragraph_text.dart';
 import 'package:tweaxy/utilities/theme_validations.dart';
 
-class AddProfilePictureWebView extends StatelessWidget {
+class AddProfilePictureWebView extends StatefulWidget {
   const AddProfilePictureWebView({super.key});
+
+  @override
+  State<AddProfilePictureWebView> createState() =>
+      _AddProfilePictureWebViewState();
+}
+
+class _AddProfilePictureWebViewState extends State<AddProfilePictureWebView> {
+  XFile? image;
+  Future pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    try {
+      final XFile? imageTemporary =
+          await picker.pickImage(source: ImageSource.gallery);
+      setState(() {
+        image = imageTemporary;
+      });
+    } catch (e) {
+      setState(() {
+        image = null;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,25 +77,67 @@ class AddProfilePictureWebView extends StatelessWidget {
                           vertical: MediaQuery.of(context).size.height * 0.02),
                     ),
                     Center(
-                      child: Stack(alignment: Alignment.center, children: [
-                        const Icon(
-                          Icons.account_circle,
-                          size: 200,
-                          color: Color(0xFF8d949a),
-                        ),
-                        MaterialButton(
-                          onPressed: () {
-                            //TODO handle image upload
-                          },
-                          color: Colors.transparent,
-                          child: const Icon(
-                            Icons.add_a_photo_outlined,
-                            size: 25,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          image != null
+                              ? CircleAvatar(
+                                  radius: 100,
+                                  backgroundImage: NetworkImage(image!.path),
+                                )
+                              : const Icon(
+                                  Icons.account_circle,
+                                  size: 200,
+                                  color: Color(0xFF8d949a),
+                                ),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Tooltip(
+                                message: 'Add photo',
+                                child: MaterialButton(
+                                  key: const ValueKey("uploadProfilePicButton"),
+                                  onPressed: () async {
+                                    await pickImage();
+                                  },
+                                  color: const Color.fromRGBO(38, 47, 53, 150),
+                                  padding: const EdgeInsets.all(20),
+                                  shape: const CircleBorder(),
+                                  child: const Icon(
+                                    color: Colors.white,
+                                    Icons.add_a_photo_outlined,
+                                    size: 25,
+                                  ),
+                                ),
+                              ),
+                              image != null
+                                  ? Tooltip(
+                                      message: 'Cancel',
+                                      child: MaterialButton(
+                                        key: const ValueKey(
+                                            "cancelProfilePicButton"),
+                                        onPressed: () {
+                                          setState(() {
+                                            image = null;
+                                          });
+                                        },
+                                        color: const Color.fromRGBO(
+                                            38, 47, 53, 100),
+                                        padding: const EdgeInsets.all(20),
+                                        shape: const CircleBorder(),
+                                        child: const Icon(
+                                          color: Colors.white,
+                                          Icons.close_sharp,
+                                          size: 25,
+                                        ),
+                                      ),
+                                    )
+                                  : Container(),
+                            ],
                           ),
-                          padding: const EdgeInsets.all(20),
-                          shape: const CircleBorder(),
-                        )
-                      ]),
+                        ],
+                      ),
                     ),
                   ],
                 ),
@@ -81,8 +146,10 @@ class AddProfilePictureWebView extends StatelessWidget {
                 width: MediaQuery.of(context).size.width * 0.3,
                 child: CustomButton(
                   key: const ValueKey("addUsernameWebSkipButton"),
-                  color: backgroundColorTheme(context),
-                  text: "Skip for now",
+                  color: image == null
+                      ? backgroundColorTheme(context)
+                      : forgroundColorTheme(context),
+                  text: image == null ? "Skip for now" : "Next",
                   onPressedCallback: () {
                     showDialog(
                       context: context,
