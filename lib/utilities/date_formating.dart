ed625a1b9e076864a98e4bb5ac7c08df93d4bc0d
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2023-11-11 19:03:19 +0200

    [🎨 REF ] resolving folders names

--- lib/utilities/date_formating.dart
+++
@@ -1,6 +0,0 @@
-import 'package:intl/intl.dart';
-
-String dateFormating({required year, required month, required day}) {
-  DateTime date = DateTime(year, month, day);
-  return DateFormat.yMMMd().format(date);
-}
