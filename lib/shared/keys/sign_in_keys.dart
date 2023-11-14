65c80bd6eb61b737b6e43de666b1ca82d6a15016
ibraam <ibraam.nashaat2@gmail.com>
2023-11-14 13:19:53 +0200

    [FR] Added some forget password keys

--- lib/shared/keys/sign_in_keys.dart
+++ lib/shared/keys/sign_in_keys.dart
@@ -3,3 +3,6 @@ const emailTextFieldKey = 'emailTextFieldKey';
 const logInView1NextButtonKey = 'logInView1NextButtonKey';
 const passwordTextFieldKey = 'passwordTextFieldKey';
 const logInView2NextButtonKey = 'logInView2NextButtonKey';
+const loginView1ForgetPassButtonKey = 'loginView1ForgetPassButton';
+const forgetPassEmailTextFieldKey = 'forgetPassView1TextField';
+const forgetPassView1NextButtonKey = 'forgetPassView1NextButton';
