23bb7ee0017838c491541d5e744c299856efd5ac
Menna-Ahmed7 <mennaahmed0701@gmail.com>
2023-11-29 19:49:45 +0200

    Merge branch 'Development' of https://github.com/TweaXy/cross-platform into Development

---
+++ test/add_tweet_api_test.dart
@@ -0,0 +1,18 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+import 'package:tweaxy/services/add_tweet.dart';
+
+void main() {
+  AddTweet service = AddTweet(Dio());
+  group('Test add tweet Api', () {
+    test('Test1: post empty tweet', () async {
+      expect(await service.addTweet("",[]), "tweet can not be empty.");
+    });
+
+  log('\n');
+    
+  });
+}
