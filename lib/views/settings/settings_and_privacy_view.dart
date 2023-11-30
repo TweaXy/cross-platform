2c1557f7b6a7ce7781ceb5eaea95b8d0ecb265f5
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2023-11-30 22:52:30 +0200

    [➕ FR ] added username to settings screens

--- lib/views/settings/settings_and_privacy_view.dart
+++ lib/views/settings/settings_and_privacy_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tweaxy/components/AppBar/settings_appbar.dart';
 import 'package:tweaxy/components/settings/custom_settings_list_tile.dart';
 import 'package:tweaxy/models/app_icons.dart';
+import 'package:tweaxy/services/temp_user.dart';
 
 class SettingsAndPrivacyView extends StatelessWidget {
   const SettingsAndPrivacyView({super.key});
@@ -9,9 +10,9 @@ class SettingsAndPrivacyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: const SettingsAppBar(
+        appBar: SettingsAppBar(
           title: "Settings",
-          username: "@username",
+          username: '@${TempUser.username}',
         ),
         body: Padding(
           padding: const EdgeInsets.only(top: 8.0),
