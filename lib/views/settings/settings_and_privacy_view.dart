21345354a03b328958c61fb5305613f9152fcd7b
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2023-12-01 13:40:00 +0200

    [🎨 REF] removed unused imports

--- lib/views/settings/settings_and_privacy_view.dart
+++ lib/views/settings/settings_and_privacy_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:tweaxy/components/AppBar/settings_appbar.dart';
 import 'package:tweaxy/components/settings/update_email_components/custom_data_display.dart';
 import 'package:tweaxy/components/transition/custom_page_route.dart';
-import 'package:tweaxy/constants.dart';
 import 'package:tweaxy/models/app_icons.dart';
 import 'package:tweaxy/views/settings/settings_view.dart';
 
