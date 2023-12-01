14119462c5f4afd8287538d07502d5d8a7951411
kaokb <kaokabkareem@gmail.com>
2023-12-01 10:30:01 +0200

    Adding Sign in with google

--- lib/components/sign_choose.dart
+++ lib/components/sign_choose.dart
@@ -6,6 +6,8 @@ import 'package:sign_button/sign_button.dart';
 import 'package:tweaxy/components/sign_in_with.dart';
 import 'package:tweaxy/components/start_screen_divider.dart';
 import 'package:tweaxy/components/toasts/custom_web_toast.dart';
+import 'package:tweaxy/constants.dart';
+import 'package:tweaxy/services/sign_in_google.dart';
 
 class SignChoose extends StatefulWidget {
   const SignChoose({
@@ -21,87 +23,15 @@ class SignChoose extends StatefulWidget {
 
 class _SignChooseState extends State<SignChoose> {
 ///////////////////////////////
-  // void signInWithGoogle() async {
-  //   try {
-  //     var googleAccount = await GoogleSingInApi.loginWeb();
-  //     if (googleAccount != null) {
-  //       var googleToken = await GoogleSingInApi.getGoogleToken();
-  //       debugPrint("google account ${googleAccount.email}");
-  //       if (googleToken != null) {
-  //         debugPrint("Google Token: $googleToken");
-  //       } else {
-  //         debugPrint("token is null");
-  //         ScaffoldMessenger.of(context).showSnackBar(
-  //           SnackBar(
-  //             content: Row(
-  //               children: [
-  //                 const Icon(
-  //                   Icons.error,
-  //                   color: Colors.red,
-  //                 ),
-  //                 SizedBox(
-  //                   width: MediaQuery.of(context).size.width * 0.01,
-  //                 ),
-  //                 const Text(
-  //                   "Error in Signing in with Google",
-  //                   style: TextStyle(
-  //                     color: Colors.red,
-  //                   ),
-  //                 ),
-  //               ],
-  //             ),
-  //           ),
-  //         );
-  //       }
-  //     } else {
-  //       debugPrint("google account is null");
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         SnackBar(
-  //           content: Row(
-  //             children: [
-  //               const Icon(
-  //                 Icons.error,
-  //                 color: Colors.red,
-  //               ),
-  //               SizedBox(
-  //                 width: MediaQuery.of(context).size.width * 0.01,
-  //               ),
-  //               const Text(
-  //                 "Error in Signing in with Google",
-  //                 style: TextStyle(
-  //                   color: Colors.red,
-  //                 ),
-  //               ),
-  //             ],
-  //           ),
-  //         ),
-  //       );
-  //     }
-  //   } catch (e) {
-  //     debugPrint("error in google sign in $e");
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       SnackBar(
-  //         content: Row(
-  //           children: [
-  //             const Icon(
-  //               Icons.error,
-  //               color: Colors.red,
-  //             ),
-  //             SizedBox(
-  //               width: MediaQuery.of(context).size.width * 0.01,
-  //             ),
-  //             const Text(
-  //               "Error in Signing in with Google",
-  //               style: TextStyle(
-  //                 color: Colors.red,
-  //               ),
-  //             ),
-  //           ],
-  //         ),
-  //       ),
-  //     );
-  //   }
-  // }
+  Future SignIn() async {
+    final user = await GoogleSignINApi.login();
+    if (user == null) {
+      CustomWebToast(message: "errrrrrrrrrrrrrrrrrrrrrr");
+    } else {
+      Navigator.of(context).popUntil((route) => route.isFirst);
+      Navigator.of(context).pushReplacementNamed(kHomeScreen);
+    }
+  }
 
 ///////////////////
   @override
@@ -114,27 +44,9 @@ class _SignChooseState extends State<SignChoose> {
             type: widget.isDarkMode ? ButtonType.googleDark : ButtonType.google,
             onPressed: () async {
               try {
-                // signInWithGoogle();
-                //   Map<String, dynamic> loginGoogle =
-                //       await LoginApi().SignInWithGoogle();
-                //   if (loginGoogle['status'] == "success") {
-                //     //go to home page
-                //     print(loginGoogle);
-                //     Navigator.pushNamed(context, kStartScreen);
-                //   }
-                // } on DioException catch (e) {
-                //   //    print('DioException: ${e.toString()}');
-                //   // ignore: use_build_context_synchronously
-                //   // showSnackBar(context, e.response!.data['message']);
-                //   showToastWidget(
-                //     CustomWebToast(
-                //       message: e.response!.data['message'],
-                //     ),
-                //     position: ToastPosition.bottom,
-                //     duration: const Duration(seconds: 2),
-                //   );
+                SignIn();
               } on Exception catch (e) {
-                //   print(e.toString());
+                print(e.toString());
                 if (kIsWeb) {
                   showToastWidget(
                     CustomWebToast(
@@ -157,28 +69,6 @@ class _SignChooseState extends State<SignChoose> {
             size: ButtonSize.medium,
           ),
         ),
-        // SizedBox(
-        //   width: double.infinity,
-        //   child: SignWithButton(
-        //     type: !isDarkMode ? ButtonType.facebook : ButtonType.facebookDark,
-        //     onPressed: () {
-        //       //TODO: implement continue with facebook logic
-        //     },
-        //     size: ButtonSize.large,
-        //   ),
-        // ),
-        // SizedBox(
-        //   width: double.infinity,
-        //   child: SignWithButton(
-        //     type: !isDarkMode ? ButtonType.github : ButtonType.githubDark,
-        //     onPressed: () {
-        //       //TODO: implement continue with github logic
-        //       var res = SignInServices.signInGithub();
-        //       // print("sign in" + res.toString());
-        //     },
-        //     size: ButtonSize.medium,
-        //   ),
-        // ),
         StartScreenDivider(isDarkMode: widget.isDarkMode),
       ],
     );
