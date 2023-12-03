157f662f5a2bc4c96a5c61f569442d1a4ceeed95
kaokb <kaokabkareem@gmail.com>
2023-12-03 14:56:36 +0200

    [🎨 REF ] refactor

--- lib/services/sign_in_google.dart
+++ lib/services/sign_in_google.dart
@@ -2,13 +2,13 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:tweaxy/components/toasts/custom_web_toast.dart';
 
 class GoogleSignINApi {
-  // static final clintId =
-  //     '700717640171-iuhebo2m8jvqf8msk87qt30fr2hrp4co.apps.googleusercontent.com';
-  static final _googleSignIn = GoogleSignIn();
+  static final clintId =
+      '700717640171-iuhebo2m8jvqf8msk87qt30fr2hrp4co.apps.googleusercontent.com';
+  static final _googleSignInMob = GoogleSignIn(clientId: clintId);
   static late GoogleSignInAuthentication googleSignInAuthentication;
   static Future<GoogleSignInAccount?> login() async {
     try {
-      final GoogleSignInAccount? user = await _googleSignIn.signIn();
+      final GoogleSignInAccount? user = await _googleSignInMob.signIn();
       if (user == null) {
         CustomWebToast(message: "Failed to sign in");
       } else {
@@ -21,14 +21,21 @@ class GoogleSignINApi {
       return null;
     }
   }
-static Future<void> getToken() async {
-    try {
-      GoogleSignInAccount? user = await _googleSignIn.signIn();
+// static Future<void> getToken() async {
+//     try {
+//       GoogleSignInAccount? user = await _googleSignIn.signIn();
 
-      googleSignInAuthentication = await user!.authentication;
-    } catch (error) {
-      print('error in getting the token\n\n\n');
-      print('error');
-    }
+//       googleSignInAuthentication = await user!.authentication;
+//     } catch (error) {
+//       print('error in getting the token\n\n\n');
+//       print('error');
+//     }
+//   }
+  static Future<String?> getGoogleToken() async {
+    String? token;
+
+    token = await _googleSignInMob.currentUser?.authentication
+        .then((value) => value.idToken);
+    return token;
   }
 }
