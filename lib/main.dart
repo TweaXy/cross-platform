93bf5fdbf81c3da169f0aea0ba141c4522ffd65e
kaokb <kaokabkareem@gmail.com>
2023-12-04 17:04:08 +0200

    Refactors

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tweaxy/cubits/updata/updata_cubit.dart';
 import 'package:tweaxy/views/profile/profile_likes.dart';
 import 'package:tweaxy/views/settings/settings_view.dart';
 import 'package:tweaxy/views/settings/settings_and_privacy_view.dart';
@@ -40,48 +41,51 @@ class TweaXy extends StatelessWidget {
     bool isDarkMode = brightness == Brightness.dark;
     return OKToast(
       child: BlocProvider(
-        create: (context) => EditProfileCubit(),
-        child: MaterialApp(
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            bottomSheetTheme:
-                const BottomSheetThemeData(backgroundColor: Colors.white),
-            brightness: brightness,
-            fontFamily: 'Roboto',
-            useMaterial3: false,
-            scaffoldBackgroundColor: isDarkMode ? Colors.black : Colors.white,
-            dialogBackgroundColor: isDarkMode ? Colors.black : Colors.white,
+        create: (context) => UpdateAllCubit(),
+        child: BlocProvider(
+          create: (context) => EditProfileCubit(),
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              bottomSheetTheme:
+                  const BottomSheetThemeData(backgroundColor: Colors.white),
+              brightness: brightness,
+              fontFamily: 'Roboto',
+              useMaterial3: false,
+              scaffoldBackgroundColor: isDarkMode ? Colors.black : Colors.white,
+              dialogBackgroundColor: isDarkMode ? Colors.black : Colors.white,
+            ),
+            routes: {
+              kSplashScreen: (context) => const SplashScreen(),
+              kStartScreen: (context) => const StartScreen(),
+              kWebStartScreen: (context) => const WebStartScreen(),
+              kLogin1Screen: (context) => const LoginViewPage1(),
+              kCreateAcountScreen: (context) => const CreateAccountView(),
+              kCreateAcountWebScreen: (context) => const CreateAccountWebView(),
+              kAuthenticationScreen: (context) => AuthenticationView(),
+              kHomeScreen: (context) => const HomePage(),
+              kCreateAcountReviewScreen: (context) =>
+                  const CreateAccountDataReview(),
+              kProfileScreen: (context) => ProfileScreen(
+                    id: '',
+                    text: '',
+                  ),
+              kSearchScreen: (context) => const SearchScreen(),
+              kFollowers: (context) => FollowersPage(username: ''),
+              kwebboth: (context) => WebFollowersAndFollowings(
+                  username: 'karim.elsayed401_67616122'),
+              kFollowing: (context) =>
+                  FollowingPage(username: 'karim.elsayed401_67616122'),
+              kLikersInTweets: (context) =>
+                  LikersInTweet(id: 'sfr1ztrbdopvclujg0boys62a'),
+              kAccountinfo: (context) => const AccountIfoView(),
+              kSettingsAndPrivacy: (context) => const SettingsAndPrivacyView(),
+              kSettings: (context) => const SettingsView(),
+              kLikersInProfile: (context) => const ProfileLikes(),
+              kUpdatePassword: (context) => const UpdatePasswordView(),
+            },
+            initialRoute: kSplashScreen,
           ),
-          routes: {
-            kSplashScreen: (context) => const SplashScreen(),
-            kStartScreen: (context) => const StartScreen(),
-            kWebStartScreen: (context) => const WebStartScreen(),
-            kLogin1Screen: (context) => const LoginViewPage1(),
-            kCreateAcountScreen: (context) => const CreateAccountView(),
-            kCreateAcountWebScreen: (context) => const CreateAccountWebView(),
-            kAuthenticationScreen: (context) => AuthenticationView(),
-            kHomeScreen: (context) => const HomePage(),
-            kCreateAcountReviewScreen: (context) =>
-                const CreateAccountDataReview(),
-            kProfileScreen: (context) => ProfileScreen(
-                  id: '',
-                  text: '',
-                ),
-            kSearchScreen: (context) => const SearchScreen(),
-            kFollowers: (context) => FollowersPage(username: ''),
-            kwebboth: (context) => WebFollowersAndFollowings(
-                username: 'karim.elsayed401_67616122'),
-            kFollowing: (context) =>
-                FollowingPage(username: 'karim.elsayed401_67616122'),
-            kLikersInTweets: (context) =>
-                LikersInTweet(id: 'sfr1ztrbdopvclujg0boys62a'),
-            kAccountinfo: (context) => const AccountIfoView(),
-            kSettingsAndPrivacy: (context) => const SettingsAndPrivacyView(),
-            kSettings: (context) => const SettingsView(),
-            kLikersInProfile: (context) => const ProfileLikes(),
-            kUpdatePassword: (context) => const UpdatePasswordView(),
-          },
-          initialRoute: kSplashScreen,
         ),
       ),
     );
--- lib/views/followersAndFollowing/likers_in_tweet.dart
+++ lib/views/followersAndFollowing/likers_in_tweet.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tweaxy/components/HomePage/floating_action_button.dart';
 import 'package:tweaxy/components/custom_followers.dart';
 import 'package:tweaxy/components/showallFollowers.dart';
 import 'package:tweaxy/components/toasts/custom_toast.dart';
 import 'package:tweaxy/components/toasts/custom_web_toast.dart';
+import 'package:tweaxy/cubits/updata/updata_cubit.dart';
+import 'package:tweaxy/cubits/updata/updata_states.dart';
 import 'package:tweaxy/models/followers_model.dart';
 import 'package:tweaxy/services/get_likers.dart';
 import 'package:tweaxy/views/followersAndFollowing/custom_future.dart';
+import 'package:tweaxy/views/loading_screen.dart';
 
 class LikersInTweet extends StatefulWidget {
   LikersInTweet({super.key, required this.id});
@@ -33,6 +37,7 @@ class _LikersInTweetState extends State<LikersInTweet> {
   @override
   void initState() {
     super.initState();
+    alllikers = {};
     controller.addListener(() {
       if (controller.position.maxScrollExtent == controller.offset) {
         setState(() {
@@ -44,66 +49,77 @@ class _LikersInTweetState extends State<LikersInTweet> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.black,
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: const Text(
-          'Liked by',
-          style: TextStyle(
-              color: Colors.black, fontWeight: FontWeight.w500, fontSize: 25),
-        ),
-        backgroundColor: Colors.white,
-        elevation: 1,
-      ),
-      body: RefreshIndicator(
-          onRefresh: _refresh,
-          child: FutureBuilder<List<FollowersModel>>(
-            future: Likers()
-                .getLikers(scroll: controller, id: widget.id, offset: offset),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                if (snapshot.data!.isEmpty && FirstTime) {
-                  return const Center(
-                    child: Text("NO Likers yet",
-                        style: TextStyle(
-                            color: Colors.black, fontWeight: FontWeight.bold)),
-                  );
-                } else {
-                  FirstTime = false;
-                  if (snapshot.data!.isNotEmpty)
-                    alllikers.addAll(snapshot.data!);
-                  myindex = alllikers.length;
-                  return ListView.builder(
-                    controller: controller,
-                    itemBuilder: (context, index) {
-                      List<FollowersModel> myList = alllikers.toList();
-                      return CustomFollowers(
-                        user: myList[index],
-                        isFollower: false,
-                      );
-                    },
-                    itemCount: alllikers.length,
-                  );
-                }
-              } else if (snapshot.hasError) {
-                return kIsWeb
-                    ? const CustomWebToast(message: "We have a problem")
-                    : const Center(
-                        child: CustomToast(message: "We have a problem"));
-              } else {
-                return const Center(child: CircularProgressIndicator());
-              }
-            },
-          )),
-      floatingActionButton: const FloatingButton(),
+    return BlocBuilder<UpdateAllCubit, UpdataAllState>(
+      builder: (context, state) {
+        if (state is LoadingStata) {
+          alllikers.clear();
+          return LoadingScreen(asyncCall: true);
+        } else
+          return Scaffold(
+            appBar: AppBar(
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              title: const Text(
+                'Liked by',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 25),
+              ),
+              backgroundColor: Colors.white,
+              elevation: 1,
+            ),
+            body: RefreshIndicator(
+                onRefresh: _refresh,
+                child: FutureBuilder<List<FollowersModel>>(
+                  future: Likers().getLikers(
+                      scroll: controller, id: widget.id, offset: offset),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      if (snapshot.data!.isEmpty && FirstTime) {
+                        return const Center(
+                          child: Text("NO Likers yet",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold)),
+                        );
+                      } else {
+                        FirstTime = false;
+                        if (snapshot.data!.isNotEmpty)
+                          alllikers.addAll(snapshot.data!);
+                        myindex = alllikers.length;
+                        return ListView.builder(
+                          controller: controller,
+                          itemBuilder: (context, index) {
+                            List<FollowersModel> myList = alllikers.toList();
+                            return CustomFollowers(
+                              user: myList[index],
+                              isFollower: false,
+                            );
+                          },
+                          itemCount: alllikers.length,
+                        );
+                      }
+                    } else if (snapshot.hasError) {
+                      return kIsWeb
+                          ? const CustomWebToast(message: "We have a problem")
+                          : const Center(
+                              child: CustomToast(message: "We have a problem"));
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  },
+                )),
+            floatingActionButton: const FloatingButton(),
+          );
+      },
     );
   }
 }
