f8d02b0537e46c088d16d5ddb7a913bc2491f27c
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2023-12-04 11:25:04 +0200

    [🐞 BUG] fixing duplicated dep

--- lib/views/profile/likers_profile_view.dart
+++ lib/views/profile/likers_profile_view.dart
@@ -1,62 +1,62 @@
-import 'package:flutter/material.dart';
-import 'package:tweaxy/components/HomePage/Tweet/tweet.dart';
-import 'package:tweaxy/models/tweet.dart';
-import 'package:tweaxy/services/Tweets.dart';
+// import 'package:flutter/material.dart';
+// import 'package:tweaxy/components/HomePage/Tweet/tweet.dart';
+// import 'package:tweaxy/models/tweet.dart';
+// import 'package:tweaxy/services/Tweets.dart';
 
-class LikersProfileView extends StatelessWidget {
-  const LikersProfileView({super.key, required this.controller});
-  final ScrollController controller;
-  List<Tweet> initializeTweets(List<Map<String, dynamic>> temp) {
-    // print('hhh' + temp.toString());
-    return temp
-        .map((e) => Tweet(
-              id: e['id']!,
-              image: e['image'],
-              userImage: e['userImage']!,
-              userHandle: e['userHandle']!,
-              userName: e['userName']!,
-              time: e['time']!,
-              tweetText: e['tweetText'],
-              userId: e['userid'],
-              likesCount: 1,
-              viewsCount: 1,
-              retweetsCount: 1,
-              commentsCount: 1,
-            ))
-        .toList();
-  }
+// class LikersProfileView extends StatelessWidget {
+//   const LikersProfileView({super.key, required this.controller});
+//   final ScrollController controller;
+//   List<Tweet> initializeTweets(List<Map<String, dynamic>> temp) {
+//     // print('hhh' + temp.toString());
+//     return temp
+//         .map((e) => Tweet(
+//               id: e['id']!,
+//               image: e['image'],
+//               userImage: e['userImage']!,
+//               userHandle: e['userHandle']!,
+//               userName: e['userName']!,
+//               time: e['time']!,
+//               tweetText: e['tweetText'],
+//               userId: e['userid'],
+//               likesCount: 1,
+//               viewsCount: 1,
+//               retweetsCount: 1,
+//               commentsCount: 1,
+//             ))
+//         .toList();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: Tweets.getTweetsHome(scroll: controller),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData || snapshot.hasError || snapshot.hasError) {
-            return const SliverToBoxAdapter(
-              child:  Center(
-                heightFactor: 3,
-                child: CircularProgressIndicator(
-                  color: Colors.blue,
-                ),
-              ),
-            );
-          } else {
-            List<Map<String, dynamic>> s = snapshot.data!;
-            // print(s);
-            List<Tweet> tweets = initializeTweets(s);
-            return SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (BuildContext context, int index) {
-                  return CustomTweet(
-                    forProfile: true,
-                    tweet: tweets[index],
-                  );
-                },
-                childCount: snapshot.data!.length,
-              ),
-            );
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//         future: Tweets.getTweetsHome(scroll: controller),
+//         builder: (context, snapshot) {
+//           if (!snapshot.hasData || snapshot.hasError || snapshot.hasError) {
+//             return const SliverToBoxAdapter(
+//               child:  Center(
+//                 heightFactor: 3,
+//                 child: CircularProgressIndicator(
+//                   color: Colors.blue,
+//                 ),
+//               ),
+//             );
+//           } else {
+//             List<Map<String, dynamic>> s = snapshot.data!;
+//             // print(s);
+//             List<Tweet> tweets = initializeTweets(s);
+//             return SliverList(
+//               delegate: SliverChildBuilderDelegate(
+//                 (BuildContext context, int index) {
+//                   return CustomTweet(
+//                     forProfile: true,
+//                     tweet: tweets[index],
+//                   );
+//                 },
+//                 childCount: snapshot.data!.length,
+//               ),
+//             );
            
-          }
-        });
-  }
-}
+//           }
+//         });
+//   }
+// }
