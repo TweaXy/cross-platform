a2fbd81406c93a81f029e909de3d57cdc6dcc3c0
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2023-12-05 11:02:06 +0200

    [🎨 REF] unit test signup

--- test/signup_api_test.dart
+++ test/signup_api_test.dart
@@ -10,13 +10,15 @@ void main() {
   SignupService service = SignupService(Dio());
   group('Test Signup Api', () {
     test('Test1: Check Email Uniqueness For New Email Test', () async {
-      expect(await service.emailUniqueness("yara.hisham.yh@gmail.com"), null);
+      final response =
+          await service.emailUniqueness("yara.hisham.yh@gmail.com");
+      expect(response.data['status'] == 'success', true);
     });
     log('\n');
 
     test('Test2: Check Email Uniqueness For Existing Email Test', () async {
-      expect(await service.emailUniqueness("yarrahishamm61@gmail.com"),
-          "Email has been used before");
+      expect(await service.emailUniqueness("Pete@gmail.com"),
+          "email already exists");
     });
     log('\n');
 
@@ -26,12 +28,14 @@ void main() {
     });
     test('Test4: Send Email Verification Code For New Email Test', () async {
       UserSignup.setEmail = "yara.hisham.yh@gmail.com";
-
-      expect(await service.sendEmailCodeVerification(), null);
+      final response = await service.sendEmailCodeVerification();
+      expect(response.data['status'] == 'success', true);
     });
     log('\n');
 
     test('Test5: Too Much Requests Test', () async {
+      UserSignup.setEmail = "yara.hisham.yh@gmail.com";
+
       expect(await service.sendEmailCodeVerification(),
           "More than one request in less than 30 seconds");
     });
@@ -39,7 +43,7 @@ void main() {
 
     test('Test6: Send Email Verification Code For Existing Email Test',
         () async {
-      UserSignup.setEmail = "yarrahishamm61@gmail.com";
+      UserSignup.setEmail = "Pete@gmail.com";
       expect(await service.sendEmailCodeVerification(),
           "Email is already exists and verified");
     });
@@ -52,6 +56,67 @@ void main() {
           await service.sendEmailCodeVerification(), "must have email format");
     });
     log('\n');
+  });
+  test(
+      'Test8: Create Account with invalid password Test (no lower case letter)',
+      () async {
+    UserSignup.setEmail = "yara.hisham.yh@gmail.com";
+    UserSignup.setName = "Yara";
+    UserSignup.setBirthdayDate = "10-10-2001";
+    UserSignup.setPassword = "12345668T@";
+    UserSignup.setEmailVerificationToken = "74b9adc4";
+    expect(await service.createAccount(),
+        'password must contain at least 1 lower case letter');
+  });
+  test(
+      'Test9: Create Account with invalid password Test (no upper case letter)',
+      () async {
+    UserSignup.setEmail = "yara.hisham.yh@gmail.com";
+    UserSignup.setName = "Yara";
+    UserSignup.setBirthdayDate = "10-10-2001";
+    UserSignup.setPassword = "12345668t@";
+    UserSignup.setEmailVerificationToken = "74b9adc4";
+    expect(await service.createAccount(),
+        'password must contain at least 1 upper case letter');
+  });
+  test(
+      'Test10: Create Account with invalid password Test (no special character)',
+      () async {
+    UserSignup.setEmail = "yara.hisham.yh@gmail.com";
+    UserSignup.setName = "Yara";
+    UserSignup.setBirthdayDate = "10-10-2001";
+    UserSignup.setPassword = "12345668tT";
+    UserSignup.setEmailVerificationToken = "74b9adc4";
+    expect(await service.createAccount(),
+        'password must contain at least 1 special character');
+  });
+  test('Test11: Create Account with invalid verfication code', () async {
+    UserSignup.setEmail = "yara.hisham.yh@gmail.com";
+    UserSignup.setName = "Yara";
+    UserSignup.setBirthdayDate = "10-10-2001";
+    UserSignup.setPassword = "12345668tT@";
+    UserSignup.setEmailVerificationToken = "74b9c4";
+    expect(await service.createAccount(),
+        'email verification code must be 8 characters');
+  });
+
+  test('Test12: Create Account with random verfication code', () async {
+    UserSignup.setEmail = "yara.hisham.yh@gmail.com";
+    UserSignup.setName = "Yara";
+    UserSignup.setBirthdayDate = "10-10-2001";
+    UserSignup.setPassword = "12345668tT@";
+    UserSignup.setEmailVerificationToken = "18js924d";
+    expect(await service.createAccount(), 'Email Verification Code is invalid');
+  });
+
+  test('Test12: Create Account Test', () async {
+    UserSignup.setEmail = "yara.hisham.yh@gmail.com";
+    UserSignup.setName = "yara";
+    UserSignup.setBirthdayDate = "10-10-2001";
+    UserSignup.setPassword = "12345678tT@";
+    UserSignup.setEmailVerificationToken = "7ec54399";
+    final response = await service.createAccount();
+    expect(response.data['status'] == 'success', true);
   });
 }
 
