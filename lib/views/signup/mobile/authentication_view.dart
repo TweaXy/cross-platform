44492f6b4e93e6fc33e01263ba141562789040f4
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2023-12-09 23:13:40 +0200

    [🎨 REF] refactor captcha api

--- lib/views/signup/mobile/authentication_view.dart
+++ lib/views/signup/mobile/authentication_view.dart
@@ -1,8 +1,12 @@
+import 'dart:developer';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:oktoast/oktoast.dart';
 import 'package:tweaxy/components/custom_appbar.dart';
 import 'package:tweaxy/components/custom_head_text.dart';
 import 'package:tweaxy/components/custom_paragraph_text.dart';
+import 'package:tweaxy/components/toasts/custom_toast.dart';
 import 'package:tweaxy/services/signup_service.dart';
 import 'package:tweaxy/utilities/theme_validations.dart';
 import 'package:tweaxy/views/signup/mobile/not_robot_view.dart';
@@ -76,12 +80,29 @@ class AuthenticationView extends StatelessWidget {
                           javascriptChannels: {
                             JavascriptChannel(
                               name: 'Captcha',
-                              onMessageReceived: (JavascriptMessage message) {
-                                Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) =>
-                                            const NotRobotView()));
+                              onMessageReceived:
+                                  (JavascriptMessage message) async {
+                                try {
+                                  dynamic response = await service
+                                      .authentication(captcha: message.message);
+                                  if (response is String) {
+                                    showToastWidget(
+                                      CustomToast(
+                                        message: response,
+                                      ),
+                                      position: ToastPosition.bottom,
+                                      duration: const Duration(seconds: 2),
+                                    );
+                                  } else {
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                const NotRobotView()));
+                                  }
+                                } catch (e) {
+                                  log(e.toString());
+                                }
                               },
                             ),
                           },
