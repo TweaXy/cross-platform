e283765835ab28b4bac4be4195d0a1cef3d09bb4
nancyalgazzar <nancyayman89@gmail.com>
2023-12-19 23:16:48 +0200

    [➕ FR] enable notification

---
+++ lib/services/notification_settings_service.dart
@@ -0,0 +1,88 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tweaxy/constants.dart';
+import 'package:tweaxy/helpers/api.dart';
+
+class NotificationSettingsService {
+  final Dio dio;
+  NotificationSettingsService(this.dio);
+
+  Future<String> notificatioCheckState(String deviceToken) async {
+    Response response;
+    String returnvalue = "";
+    String? token;
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      token = prefs.getString('token');
+    } catch (e) {
+      log(e.toString());
+    }
+    try {
+      response = await Api.post(
+        url: '${baseURL}notification/satatus',
+        token: token,
+        body: {"token": deviceToken, "type": "android"},
+      );
+      if (response.statusCode == 200) {
+        if (response.data["data"] == "status:enabled") {
+          returnvalue = "true";
+        } else {
+          returnvalue = "false";
+        }
+      } else {
+        returnvalue = "";
+      }
+      return returnvalue;
+    } catch (e) {
+      log(e.toString());
+      throw Exception('notification State error');
+    }
+  }
+
+  Future<Response> notificatioDisable(String deviceToken) async {
+    Response response;
+    String? token;
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      token = prefs.getString('token');
+    } catch (e) {
+      log(e.toString());
+    }
+    try {
+      response = await Api.delete(
+        url: '${baseURL}notification/deviceTokenAndorid',
+        token: token,
+        body: {"token": deviceToken},
+      );
+
+      return response;
+    } catch (e) {
+      log(e.toString());
+      throw Exception('notification disable error');
+    }
+  }
+  Future<Response> notificatioEnable(String deviceToken) async {
+    Response response;
+    String? token;
+    try {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      token = prefs.getString('token');
+    } catch (e) {
+      log(e.toString());
+    }
+    try {
+      response = await Api.delete(
+        url: '${baseURL}notification/deviceTokenAndorid',
+        token: token,
+        body: {"token": deviceToken},
+      );
+
+      return response;
+    } catch (e) {
+      log(e.toString());
+      throw Exception('notification enable error');
+    }
+  }
+}
--- lib/views/settings/settings_and_privacy_view.dart
+++ lib/views/settings/settings_and_privacy_view.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:tweaxy/components/AppBar/settings_appbar.dart';
 import 'package:tweaxy/components/settings/update_email_components/custom_data_display.dart';
 import 'package:tweaxy/components/transition/custom_page_route.dart';
 import 'package:tweaxy/models/app_icons.dart';
 import 'package:tweaxy/shared/keys/settings_keys.dart';
 import 'package:tweaxy/views/settings/mutes_and_blocks/settings_privacy_safety_screen.dart';
-import 'package:tweaxy/views/settings/notification_settings/notification_settings_view.dart';
+// import 'package:tweaxy/views/settings/notification_settings/notification_settings_view.dart';
 import 'package:tweaxy/views/settings/settings_view.dart';
 
 class SettingsAndPrivacyView extends StatelessWidget {
-  const SettingsAndPrivacyView({super.key});
+  SettingsAndPrivacyView({super.key});
 
+  bool notiifcationEnable = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,20 +43,20 @@ class SettingsAndPrivacyView extends StatelessWidget {
                     },
                   ),
                 ),
-                CustomdataDisplay(
-                  key: const ValueKey(SettingsKeys.notifications),
-                  lead: const Icon(AppIcon.notification),
-                  title: "Notifications",
-                  subtitle:
-                      "Select the kinds of notifications you get about your activity,intrests and recommendations.",
-                  onpress: () {
-                    Navigator.push(
-                        context,
-                        CustomPageRoute(
-                            direction: AxisDirection.left,
-                            child: const NoticicationSettingsView()));
-                  },
-                ),
+                // CustomdataDisplay(
+                //   key: const ValueKey(SettingsKeys.notifications),
+                //   lead: const Icon(AppIcon.notification),
+                //   title: "Notifications",
+                //   subtitle:
+                //       "Select the kinds of notifications you get about your activity,intrests and recommendations.",
+                //   onpress: () {
+                //     Navigator.push(
+                //         context,
+                //         CustomPageRoute(
+                //             direction: AxisDirection.left,
+                //             child: const NoticicationSettingsView()));
+                //   },
+                // ),
                 Padding(
                   padding: EdgeInsets.only(
                       top: MediaQuery.of(context).size.height * .02),
@@ -71,6 +73,13 @@ class SettingsAndPrivacyView extends StatelessWidget {
                               child: const SettingsPrivacySafetyScreen()));
                     },
                   ),
+                ),
+                SwitchListTile(
+                  title: const Text('Notification Enable'),
+                  value: notiifcationEnable,
+                  onChanged: (bool value) {
+
+                  },
                 ),
               ],
             ),
