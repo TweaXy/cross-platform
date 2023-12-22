130e2ef200ec5abc12d9f850c342494a6956f7e6
Menna-Ahmed7 <mennaahmed0701@gmail.com>
2023-12-22 13:49:45 +0200

    [🎨 REF] refactoring

--- lib/components/Profile/profile_likes.dart
+++ lib/components/Profile/profile_likes.dart
@@ -8,6 +8,7 @@ import 'package:tweaxy/cubits/Tweets/tweet_states.dart';
 import 'package:tweaxy/models/tweet.dart';
 import 'package:tweaxy/services/get_likers_in_profile.dart';
 import 'package:tweaxy/services/temp_user.dart';
+import 'package:tweaxy/utilities/tweets_utilities.dart';
 
 class ProfileLikes extends StatefulWidget {
   const ProfileLikes({
@@ -68,21 +69,10 @@ class _ProfileLikesState extends State<ProfileLikes> {
       if (state is TweetDeleteState || state is TweetAddedState) {
         _pagingController.refresh();
       }
-        if (state is TweetLikedState) {
-              _pagingController.itemList!.map((element) {
-                if (element.id == state.parentid) {
-                  element.isUserLiked = !element.isUserLiked;
-                  element.likesCount++;
-                }
-                return element;
-              }).toList();
-
-        BlocProvider.of<TweetsUpdateCubit>(context).initializeTweet();
-      }
-
+      updateStatesforTweet(state, context, _pagingController);
       if (state is TweetUnLikedState && TempUser.id == widget.id) {
         _pagingController.itemList!
-            .removeWhere((element) => element.id == state.parentid);
+            .removeWhere((element) => element.id == state.id);
         // BlocProvider.of<TweetsUpdateCubit>(context).initializeTweet();
       }
       return PagedSliverList<int, Tweet>(
